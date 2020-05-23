e05cc4e35f22e4982c819633371da76e7e9c6a92
Navid <nkhalili@live.com>
2020-05-22 20:42:07 -0400

    Show components in App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import { hot } from 'react-hot-loader';
+import TodoList from './todos/TodoList';
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
+      <TodoList />
     </div>
   );
 };
--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.css';
+import NewTodoForm from './NewTodoForm';
 
-const TodoList = ({ todos }) => (
+const TodoList = ({ todos = [{ text: 'Hello' }] }) => (
   <div className="list-wrapper">
+    <NewTodoForm />
     {todos.map((todo) => (
       <TodoListItem todo={todo} />
     ))}

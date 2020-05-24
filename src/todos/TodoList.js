d1a49d3a1e9da8e7ffd48745c26dbcbcc565972e
Navid Khalili <nkhalili@live.com>
2020-05-23 21:55:25 -0400

    Connect TodoList n TodoListItem to store

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -2,14 +2,24 @@ import React from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.css';
 import NewTodoForm from './NewTodoForm';
+import { connect } from 'react-redux';
+import { removeTodo } from './actions';
 
-const TodoList = ({ todos = [{ text: 'Hello' }] }) => (
+const TodoList = ({ todos = [], onRemovePressed }) => (
   <div className="list-wrapper">
     <NewTodoForm />
     {todos.map((todo) => (
-      <TodoListItem todo={todo} />
+      <TodoListItem todo={todo} onRemovePressed={onRemovePressed} />
     ))}
   </div>
 );
 
-export default TodoList;
+const mapStateToProps = (state) => ({
+  todos: state.todos,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onRemovePressed: (text) => dispatch(removeTodo(text)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);

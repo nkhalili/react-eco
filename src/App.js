72c2878dcc566a3217faabd07d55a3968e920c31
Navid Khalili <nkhalili@live.com>
2020-05-31 19:27:31 -0400

    Used styled components for the rest

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import './App.css';
 import { hot } from 'react-hot-loader';
 import TodoList from './todos/TodoList';
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+  margin: 1rem;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #222222;
+`;
 
 const App = () => {
   return (
-    <div className="App">
+    <AppContainer>
       <TodoList />
-    </div>
+    </AppContainer>
   );
 };
 

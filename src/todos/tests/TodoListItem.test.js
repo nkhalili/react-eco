5cae55b27e7e33724426808e437d27abe12aec75
Navid Khalili <nkhalili@live.com>
2020-06-01 10:55:05 -0400

    Testing styled-components

---
+++ src/todos/tests/TodoListItem.test.js
@@ -0,0 +1,25 @@
+import { expect } from 'chai';
+import { getBorderStyleForDate } from '../TodoListItem';
+
+// We test the logic inside the styled-component and not the whole component
+describe('getBorderStyleForDate', () => {
+  it('returns none when the date is less than five days ago', () => {
+    const today = Date.now();
+    const recentDate = new Date(Date.now() - 86400000 * 3);
+
+    const expected = 'none';
+    const actual = getBorderStyleForDate(recentDate, today);
+
+    expect(actual).to.equal(expected);
+  });
+
+  it('returns a border when the date is more than five days ago', () => {
+    const today = Date.now();
+    const recentDate = new Date(Date.now() - 86400000 * 7);
+
+    const expected = '2px solid red';
+    const actual = getBorderStyleForDate(recentDate, today);
+
+    expect(actual).to.equal(expected);
+  });
+});
